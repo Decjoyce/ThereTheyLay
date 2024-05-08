0df3a2d1c98d9cd2f59bfc4d9df920648fcf3dc6
LilGenius03 <finnarcher03@gmail.com>
2024-05-08 19:23:25 +0100

    Main menu lets go

---
+++ Assets/Main_Menu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Main_Menu : MonoBehaviour
+{
+    [SerializeField] private string PasswordText;
+
+    [SerializeField] private string[] PasswordOptions;
+    [SerializeField] private TextMeshProUGUI ButtonText;
+
+    [SerializeField] private GameObject SettingsMenuButton;
+    [SerializeField] private GameObject PlayMenuButton;
+    [SerializeField] private GameObject NoResButton;
+
+    private void Start()
+    {
+        PasswordOptions[0] = "Settings";
+        PasswordOptions[1] = "Play";
+    }
+    private void Update()
+    {
+        if(PasswordText == PasswordOptions[0])
+        {
+            SettingsMenuButton.SetActive(true);
+            PlayMenuButton.SetActive(false);
+            NoResButton.SetActive(false);
+        }
+
+        else if(PasswordText == PasswordOptions[1]) 
+        {
+            SettingsMenuButton.SetActive(false);
+            PlayMenuButton.SetActive(true);
+            NoResButton.SetActive(false);
+        }
+
+        else
+        {
+            SettingsMenuButton.SetActive(false);
+            PlayMenuButton.SetActive(false);
+            NoResButton.SetActive(true);
+        }
+
+
+    }
+    public void InsertInput(string input)
+    {
+        PasswordText = input;
+        DisplayReactionText();
+    }
+
+    private void DisplayReactionText()
+    {
+        ButtonText.text = PasswordText;
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Tutorial Level");
+    }
+}
