87e38c3e6c0aba2b98ca9579d55769dc699aac01
LilGenius03 <finnarcher03@gmail.com>
2024-05-15 17:17:37 +0100

    email system added

---
+++ Assets/Email_System.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Email_System : MonoBehaviour
+{
+    [SerializeField] private GameObject EmailScreen;
+    [SerializeField] private GameObject EmailButton;
+    [SerializeField] private string Email;
+    [SerializeField] private TextMeshProUGUI text;
+    private float PopupTime;
+    [SerializeField] private AudioSource NotificationSound;
+    [SerializeField] private AudioClip Sound;   
+    void Start()
+    {
+        EmailScreen.SetActive(false);
+        EmailButton.SetActive(false);
+        PopupTime = Random.Range(3f, 12f);
+        StartCoroutine(Mail());
+    }
+
+    // Update is called once per frame
+
+
+    public void EmailPopup()
+    {
+        EmailScreen.SetActive(true);
+        text.text = Email;
+    }
+    public void OK()
+    {
+        EmailScreen.SetActive(false);
+    }
+
+    IEnumerator Mail()
+    {
+        yield return new WaitForSeconds(PopupTime);
+        NotificationSound.PlayOneShot(Sound, 1f);
+        EmailButton.SetActive(true);
+    }
+}
